06e06ee3c6171d6d4353b05a031dc36dc08e352f
Merlin Krämer <merlinkraemer@stu.deb>
2025-01-27 13:58:25 +0100

    First commit - setup dotfiles w/ stow on stu.deb

---
+++ deb-ninite.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+# Ensure the script is run with root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "Install as root!" 
+   exit 1
+fi
+
+# Update and install essential tools
+echo "Updating package list and installing dependencies..."
+apt update && apt upgrade -y
+sudo install -d -m 0755 /etc/apt/keyrings 
+sudo apt install -y software-properties-common apt-transport-https wget gpg snapd coreutils sudo bash-completion net-tools gnupg2 curl wget vim git htop lsof tree unzip zip
+sudo snap install snapd -y
+sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo apt install -y gnome-software-plugin-snap
+update-desktop-database ~/.local/share/applications/
+update-desktop-database /usr/share/applications/
+
+# Install Firefox Nightly source
+echo "Grabbing Firefox Nightly source..."
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); print "\n"$0"\n"}'
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+# Install Alacritty
+echo "Grabbing Alacritty source..."
+sudo snap install alacritty --classic
+
+# Install Chromium
+echo "Installing Chromium..."
+sudo snap install chromium
+
+# Install Thunderbird
+echo "Installing Thunderbird..."
+apt install -y thunderbird
+
+# Install Obsidian
+echo "Installing Obsidian via snap..."
+sudo snap install obsidian --classic
+
+# Install Syncthing
+echo "Installing Syncthing..."
+wget -qO- https://syncthing.net/release-key.txt | gpg --dearmor > /usr/share/keyrings/syncthing.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/syncthing.gpg] https://apt.syncthing.net/ syncthing stable" > /etc/apt/sources.list.d/syncthing.list
+
+# Start Syncthing at startup for $USER
+sudo systemctl enable syncthing@$USER.service
+
+# Install FileZilla
+echo "Installing FileZilla..."
+apt install -y filezilla
+
+# Install Bitwarden
+echo "Installing Bitwarden..."
+sudo snap install bitwarden
+
+# Install Thunar
+echo "Installing Thunar file manager..."
+apt install -y thunar
+
+#Install Gimp
+echo "Installing via flatpak..."
+echo "Gimp..."
+latpak install flathub org.gimp.GIMP -y
+echo "Chatterino..."
+flatpak install flathub com.chatterino.chatterino -y
+echo "Spotify..."
+flatpak install flathub com.spotify.Client -y
+echo "OBS..."
+flatpak install flathub com.obsproject.Studio -y
+echo "VLC..."
+flatpak install flathub org.videolan.VLC -y
+echo "Discord..."
+flatpak install flathub com.discordapp.Discord -y
+echo "WhatsApp (Whatsie)..."
+flatpak install flathub com.ktechpit.whatsie -y
+echo "Teams..."
+flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y
+echo "GoogleChrome..."
+flatpak install flathub com.google.Chrome -y
+echo "VSCode..."
+flatpak install flathub com.visualstudio.code -y
+
+
+echo "---flatpak done---"
+
+# FONT
+echo "Installing fonts..."
+echo "JetBrainsMono NerdFont & Cousine NerdFont"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Cousine.zip -O /tmp/Cousine.zip && unzip /tmp/Cousine.zip -d /tmp/Cousine && sudo cp -n /tmp/Cousine/*.ttf /usr/share/fonts/ && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip && unzip /tmp/JetBrainsMono.zip -d /tmp/JetBrainsMono && sudo cp -n /tmp/JetBrainsMono/*.ttf /usr/share/fonts/ && sudo fc-cache -fv && rm -rf /tmp/Cousine /tmp/Cousine.zip /tmp/JetBrainsMono /tmp/JetBrainsMono.zip
+
+
+#ZSH
+echo "Installing ZSH..."
+sudo apt install zsh -y
+echo "Making ZSH default Shell..."
+sudo chsh -s $(which zsh)
+# echo "Installing oh-my-zsh"
+# sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo "installing eza..."
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+echo "install zsh-auto-suggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+echo "zsh-syntax-highlighting..."
+sudo apt install zsh-syntax-highlighting
+echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+
+# Install I3, i3wm-lock
+echo "Installing I3 & i3-lock via Snap..."
+sudo apt install i3 -y
+sudo snap install i3wm-lock --edge
+sudo apt install rofi -y
+
+# Update package list and install applications
+echo "Updating package sources..."
+apt update
+
+echo "Installing packages..."
+apt install -y code google-chrome-stable syncthing firefox-nightly
+
+# Clean up temporary files
+echo "Cleaning up..."
+rm -f /tmp/firefox-nightly.tar.bz2
+
+echo "--------------------------- Done!----------------------------"
+read -p "Do you want to reboot now? (Y/N): " choice
+
+# Handle the choice
+case "$choice" in
+    [Yy]|[Yy][Ee][Ss]) # Accepts Y, y, Yes, yes
+        echo "Rebooting now..."
+        sudo reboot
+        ;;
+    [Nn]|[Nn][Oo]) # Accepts N, n, No, no
+        echo "Reboot canceled. Exiting script."
+        ;;
+    *)
+        echo "Invalid input. Please enter Y or N."
+        ;;
+esac
---
+++ zsh/.zshrc
@@ -0,0 +1,120 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+
+# Uncomment one of the following lines to change the auto-update behavior
+# zstyle ':omz:update' mode disabled  # disable automatic updates
+# zstyle ':omz:update' mode auto      # update automatically without asking
+zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# You can also set it to another string to have that shown instead of the default red dots.
+# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
+# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
+COMPLETION_WAITING_DOTS="true"
+
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
+
+ZSH_THEME="robbyrussell"
+CASE_SENSITIVE="true"
+ENABLE_CORRECTION="true"
+
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
+
+export EDITOR='code'
+
+# history setup
+HISTFILE=$HOME/.zhistory
+SAVEHIST=1000
+HISTSIZE=999
+setopt share_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_verify
+
+# completion using arrow keys (based on history)
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
+
+# -------
+# Aliases
+# -------
+alias l="ls" # List files in current directory
+alias ll="ls -al" # List all files in current directory in long list format
+alias o="open ." # Open the current directory in Finder
+
+# ----------------------
+# Path Aliases
+# ----------------------
+alias teams="cd /mnt/teams/"
+alias eh="cd /mnt/teams/Eventhandel.de/"
+# ----------------------
+# Config Aliases
+# ----------------------
+alias zshconf="vim ~/dotfiles/zsh/.zshrc"
+# ----------------------
+# Git Aliases
+# ----------------------
+alias gaa='git add .'
+alias gcm='git commit -m'
+alias gpsh='git push'
+alias gss='git status -s'
+alias gs='echo ""; echo "*********************************************"; echo -e "   DO NOT FORGET TO PULL BEFORE COMMITTING"; echo "*********************************************"; echo ""; git status'
+
+# Eza (better ls) Alias
+alias ls="eza --icons=always"
+
+unsetopt CORRECT
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# Syntax Highlighting
+source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Auto Suggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
